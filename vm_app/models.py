4050d841f71b1b1408e8053d587ee9c09f62eca0
BigFCode <ogonkovpetr3@gmail.com>
2025-12-15 09:33:06 +1100

    модель бд

--- vm_app/models.py
+++ vm_app/models.py
@@ -1,26 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-
-class User(models.Model):
-    username = models.TextField(unique=True)
-    password = models.TextField()
-
-    class Meta:
-        db_table = 'User'
-
-
-class Virtualmachine(models.Model):
-    name = models.TextField()
+class VirtualMachine(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
     class Meta:
-        db_table = 'VirtualMachine'
+        db_table = "VirtualMachine"
+
+    def __str__(self):
+        return self.name
 
 
 class Access(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    vm = models.ForeignKey(Virtualmachine, on_delete=models.CASCADE)
-    ssh_key = models.TextField()
-    pak_code = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    vm = models.ForeignKey(VirtualMachine, on_delete=models.CASCADE)
+    ssh_key = models.TextField(blank=True, null=True)
+    pak_code = models.TextField(blank=True, null=True)
+
 
     class Meta:
-        db_table = 'Access'
+        db_table = "Access"
+
+    def __str__(self):
+        return f"{self.user.username} → {self.vm.name}"
