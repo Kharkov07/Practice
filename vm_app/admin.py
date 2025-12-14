7585b16b0927d78b0f6d32889583552fe7043466
BigFCode <ogonkovpetr3@gmail.com>
2025-12-15 09:36:27 +1100

    админ

--- vm_app/admin.py
+++ vm_app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import VirtualMachine, Access
 
-# Register your models here.
+User = get_user_model()
+
+
+class AccessInlineForVM(admin.TabularInline):
+    model = Access
+    extra = 1
+    autocomplete_fields = ["user"]
+
+
+@admin.register(VirtualMachine)
+class VirtualMachineAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+    fields = ("name", "image", "description")
+    inlines = [AccessInlineForVM]
+
+
+
+class AccessInlineForUser(admin.TabularInline):
+    model = Access
+    extra = 0
+    autocomplete_fields = ["vm"]
+
+
+class AccessAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "vm", "ssh_key", "pak_code")
+    list_filter = ("user", "vm")
+    search_fields = ("user__username", "vm__name", "ssh_key", "pak_code")
+
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    inlines = [AccessInlineForUser]
+
+
+admin.site.register(Access, AccessAdmin)
